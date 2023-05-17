43f8f75b1dd35a3599e45ebd7fe8ff6a19ec28e7
CreamBBQ <jgmd1306@gmail.com>
2023-05-17 15:40:43 -0300

    header terminado esteticamente

--- JS/main.js
+++ JS/main.js
@@ -50,3 +50,37 @@ const changeLanguage = async language => {
 flagsElement.addEventListener('click', (e) => {
     changeLanguage(e.target.parentElement.dataset.language);
 })
+
+/*===== class active por secciones =====*/
+window.addEventListener('scroll', () => {
+    const scrollY = window.pageYOffset;
+    sectionAll.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 100;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
+            document.querySelector('nav a[href*=' + sectionId + ']').classList.add('active');
+        }
+        else {
+            document.querySelector('nav a[href*=' + sectionId + ']').classList.remove('active');
+        }
+    });
+});
+
+/*===== Boton y función ir arriba =====*/
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container').classList.add('show');
+    }
+    else {
+        document.querySelector('.go-top-container').classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
