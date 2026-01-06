014bfd46fb5be9c6cbed31a4a13f8571e569829a
Guillermo Muñoz <jgmd1306@gmail.com>
2026-01-06 12:44:25 -0300

    Intento 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-
 export default defineConfig({
-  site: 'https://creambbq.github.io',
-  base: '/',
-  integrations: [tailwind()]
+  // ...
+  integrations: [tailwind()],
+  // ...
 });
